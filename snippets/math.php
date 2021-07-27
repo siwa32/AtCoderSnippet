70d13f6afab503466a9c13c8eaf1da8eed11a35b
IWASE Shigeaki <siwa32@gmail.com>
2021-07-27 23:59:10 +0900

    gcd, lcm

--- snippets/math.php
+++ snippets/math.php
@@ -11,3 +11,31 @@ function intdiv_ceil(int $x, int $y): int
     return intdiv($x + $y - 1, $y);
 }
 
+/**
+ * 最大公約数を求める
+ * @param int $a
+ * @param int $b
+ * @return int
+ */
+function gcd(int $a, int $b): int
+{
+    while (true) {
+        $r = $a % $b;
+        if ($r === 0) {
+            return $b;
+        }
+        $a = $b;
+        $b = $r;
+    }
+}
+
+/**
+ * 最小公倍数を求める
+ * @param int $a
+ * @param int $b
+ * @return float|int
+ */
+function lcm(int $a, int $b)
+{
+    return $a / gcd($a, $b) * $b;
+}
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -30,4 +30,23 @@ class MathTest extends TestCase
         self::assertThat(intdiv_ceil(PHP_INT_MAX - 1, 1), self::equalTo(PHP_INT_MAX - 1));
     }
 
+    function testGcd()
+    {
+        self::assertThat(gcd(10, 2), self::equalTo(2));
+        self::assertThat(gcd(19472620, 19105), self::equalTo(5));
+        self::assertThat(gcd(19, 11), self::equalTo(1));
+        self::assertThat(gcd(17386, 17386), self::equalTo(17386));
+        self::assertThat(gcd(1, 17386), self::equalTo(1));
+        self::assertThat(gcd(17386, 1), self::equalTo(1));
+    }
+
+    function testLcm()
+    {
+        self::assertThat(lcm(1, 2), self::equalTo(2));
+        self::assertThat(lcm(7, 1), self::equalTo(7));
+        self::assertThat(lcm(6, 4), self::equalTo(12));
+        self::assertThat(lcm(10, 3), self::equalTo(30));
+        self::assertThat(lcm(17386, 17386), self::equalTo(17386));
+        self::assertThat(lcm(111, 157), self::equalTo(111 * 157));
+    }
 }
