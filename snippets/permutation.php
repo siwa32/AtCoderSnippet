3dd3fcff72fa7f3c7795de11b9a74f078a224d68
IWASE Shigeaki <siwa32@gmail.com>
2021-07-28 20:12:36 +0900

    permutation

---
+++ snippets/permutation.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * 順列
+ * 指定した配列の要素を使用して出来る辞書順の次の順列
+ *
+ * <code>
+ * foreach (next_permutation([1,2,3] as $perm) {
+ *     print_r($perm);
+ * }
+ *
+ * => [1,2,3]
+ * => [1,3,2]
+ * => [2,1,2]
+ * => [2,3,1]
+ * => [3,1,2]
+ * => [3,2,1]
+ * </code>
+ *
+ * @param array $sortedElements 昇順ソート済配列
+ * @return Generator
+ */
+function next_permutation(array $sortedElements): Generator
+{
+    $_permutation = function (array $sortedElements, int $i = 0, array $res = [], array $used = []) use (&$_permutation): Generator {
+        $elementCount = count($sortedElements);
+        foreach ($sortedElements as $item) {
+            if ($used[$item] ?? false) {
+                continue;
+            }
+            $used[$item] = true;
+            $next = $res;
+            $next[] = $item;
+            if ($i === $elementCount - 1) {
+                yield $next;
+            } else {
+                foreach ($_permutation($sortedElements, $i + 1, $next, $used) as $result) {
+                    yield $result;
+                }
+            }
+            $used[$item] = false;
+        }
+    };
+    foreach ($_permutation($sortedElements) as $item) {
+        yield $item;
+    }
+}
+
+/**
+ * 指定した配列の要素で出来る順列を辞書順に列挙する
+ *
+ * <code>
+ * $p = permutations([1,2,3]);
+ * print_r($p)
+ *
+ * => [
+ *      0 => [1,2,3],
+ *      1 => [1,3,2],
+ *      2 => [2,1,2],
+ *      3 => [2,3,1],
+ *      4 => [3,1,2],
+ *      5 => [3,2,1],
+ *    ]
+ * </code>
+ *
+ * @param array $sortedElements 昇順ソート済配列
+ * @return array
+ */
+function permutations(array $sortedElements): array
+{
+    $res = [];
+    foreach (next_permutation($sortedElements) as $permutation) {
+        $res[] = $permutation;
+    }
+    return $res;
+}
---
+++ tests/PermutationTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . "/../snippets/permutation.php";
+
+class PermutationTest extends TestCase
+{
+    function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    function testNextPermutation()
+    {
+        $expected = [
+            0 => [1,2,3],
+            1 => [1,3,2],
+            2 => [2,1,3],
+            3 => [2,3,1],
+            4 => [3,1,2],
+            5 => [3,2,1],
+        ];
+        $actual = [];
+        foreach (next_permutation([1,2,3]) as $i => $perm) {
+            $actual[] = $perm;
+        }
+        self::assertThat($actual, self::equalTo($expected));
+
+        $expected = [
+            0 => [1],
+        ];
+        $actual = [];
+        foreach (next_permutation([1]) as $i => $perm) {
+            $actual[] = $perm;
+        }
+        self::assertThat($actual, self::equalTo($expected));
+
+        $actual = [];
+        foreach (next_permutation([]) as $i => $perm) {
+            $actual[] = $perm;
+        }
+        self::assertThat($actual, self::equalTo([]));
+    }
+
+    function testPermutations()
+    {
+        $expected = [
+            0 => [1,2,3],
+            1 => [1,3,2],
+            2 => [2,1,3],
+            3 => [2,3,1],
+            4 => [3,1,2],
+            5 => [3,2,1],
+        ];
+        self::assertThat(permutations([1,2,3]), self::equalTo($expected));
+
+        $expected = [
+            0 => [1],
+        ];
+        self::assertThat(permutations([1]), self::equalTo($expected));
+
+        self::assertThat(permutations([]), self::equalTo([]));
+    }
+}
