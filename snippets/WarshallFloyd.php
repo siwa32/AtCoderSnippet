611640d6b3117fef63b641e32b4d02afc0074689
IWASE Shigeaki <siwa32@gmail.com>
2021-12-24 03:10:56 +0900

    ワーシャルフロイド法の経路修正

--- snippets/WarshallFloyd.php
+++ snippets/WarshallFloyd.php
@@ -68,7 +68,7 @@ class WarshallFloyd
                     $ij = $di[$j];
                     $ik = $di[$k];
                     $kj = $dk[$j];
-                    $this->path[$i][$j] = $j;
+                    $this->path[$i][$j] ??= $j;
                     if ($ij > $ik + $kj) {
                         $di[$j] = $ik + $kj;
                         $this->path[$i][$j] = $k;
@@ -89,7 +89,7 @@ class WarshallFloyd
         return $this->distance[$from][$to];
     }
 
-    public function isExistRoute(int $from, int $to): bool
+    public function isPathExist(int $from, int $to): bool
     {
         if ($from < 0 || $this->size <= $from) {
             throw new InvalidArgumentException();
@@ -109,8 +109,8 @@ class WarshallFloyd
             throw new InvalidArgumentException();
         }
 
-        if (!isset($this->path[$from][$to])) {
-            return [];
+        if (!$this->isPathExist($from, $to)) {
+            return [];// 経路無し
         }
 
         $res = [];
@@ -120,36 +120,11 @@ class WarshallFloyd
             if ($to === $k) {
                 $res[] = $to;
             } else {
-                $__path($from, $k, $res);
-                $__path($k, $to, $res);
+                $__path($from, $k);
+                $__path($k, $to);
             }
         };
         $__path($from, $to);
         return $res;
-    }
-}
-
-if (0) {
-    $wf = new WarshallFloyd(7, false);
-    $wf->addEdge(0, 1, 10);
-    $wf->addEdge(0, 2, 16);
-    $wf->addEdge(0, 3, 12);
-    $wf->addEdge(1, 2, 18);
-    $wf->addEdge(3, 2, 3);
-    $wf->addEdge(1, 4, 4);
-    $wf->addEdge(2, 5, 1);
-    $wf->addEdge(3, 5, 5);
-    $wf->addEdge(4, 6, 21);
-    $wf->addEdge(5, 6, 9);
-    $wf->calc();
-    for ($i = 0; $i < 7; ++$i) {
-        for ($j = 0; $j < 7; ++$j) {
-            echo "{$i} -> {$j} : " . $wf->getCost($i, $j) . " path: ";
-            $path = $wf->getPath($i, $j);
-            foreach ($path as $node) {
-                echo "{$node} ";
-            }
-            echo PHP_EOL;
-        }
     }
 }
--- tests/WarshallFloydTest.php
+++ tests/WarshallFloydTest.php
@@ -6,7 +6,6 @@ require_once __DIR__ . "/../snippets/WarshallFloyd.php";
 
 class WarshallFloydTest extends TestCase
 {
-
     /**
      * @covers WarshallFloyd::__construct
      * @covers WarshallFloyd::getSize
@@ -39,16 +38,11 @@ class WarshallFloydTest extends TestCase
         self::assertThat($target->getCost(6, 0), self::equalTo(WarshallFloyd::INF_COST));
         self::assertThat($target->getCost(6, 6), self::equalTo(WarshallFloyd::INF_COST));
         self::assertThat($target->getCost(3, 2), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(0, 0), self::isFalse());
-        self::assertThat($target->isExistRoute(0, 6), self::isFalse());
-        self::assertThat($target->isExistRoute(6, 0), self::isFalse());
-        self::assertThat($target->isExistRoute(6, 6), self::isFalse());
-        self::assertThat($target->isExistRoute(3, 2), self::isFalse());
-    }
-
-    public function testGetCost(): void
-    {
-        self::markTestIncomplete();
+        self::assertThat($target->isPathExist(0, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 6), self::isFalse());
+        self::assertThat($target->isPathExist(6, 0), self::isFalse());
+        self::assertThat($target->isPathExist(6, 6), self::isFalse());
+        self::assertThat($target->isPathExist(3, 2), self::isFalse());
     }
 
     /**
@@ -94,43 +88,43 @@ class WarshallFloydTest extends TestCase
         $target->addEdge(3, 5, 5);
         self::assertThat($target->getCost(0, 1), self::equalTo(10));
         self::assertThat($target->getCost(1, 0), self::equalTo(10));
-        self::assertThat($target->isExistRoute(0, 1), self::isTrue());
-        self::assertThat($target->isExistRoute(1, 0), self::isTrue());
+        self::assertThat($target->isPathExist(0, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 0), self::isTrue());
         self::assertThat($target->getCost(0, 2), self::equalTo(16));
         self::assertThat($target->getCost(2, 0), self::equalTo(16));
-        self::assertThat($target->isExistRoute(0, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 0), self::isTrue());
+        self::assertThat($target->isPathExist(0, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 0), self::isTrue());
         self::assertThat($target->getCost(0, 3), self::equalTo(12));
         self::assertThat($target->getCost(3, 0), self::equalTo(12));
-        self::assertThat($target->isExistRoute(0, 3), self::isTrue());
-        self::assertThat($target->isExistRoute(3, 0), self::isTrue());
+        self::assertThat($target->isPathExist(0, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 0), self::isTrue());
         self::assertThat($target->getCost(1, 2), self::equalTo(18));
         self::assertThat($target->getCost(2, 1), self::equalTo(18));
-        self::assertThat($target->isExistRoute(1, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 1), self::isTrue());
         self::assertThat($target->getCost(3, 2), self::equalTo(3));
         self::assertThat($target->getCost(2, 3), self::equalTo(3));
-        self::assertThat($target->isExistRoute(3, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 3), self::isTrue());
         self::assertThat($target->getCost(1, 4), self::equalTo(4));
         self::assertThat($target->getCost(4, 1), self::equalTo(4));
-        self::assertThat($target->isExistRoute(1, 4), self::isTrue());
-        self::assertThat($target->isExistRoute(4, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 4), self::isTrue());
+        self::assertThat($target->isPathExist(4, 1), self::isTrue());
         self::assertThat($target->getCost(2, 5), self::equalTo(1));
         self::assertThat($target->getCost(5, 2), self::equalTo(1));
-        self::assertThat($target->isExistRoute(2, 5), self::isTrue());
-        self::assertThat($target->isExistRoute(5, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 2), self::isTrue());
         self::assertThat($target->getCost(3, 5), self::equalTo(5));
         self::assertThat($target->getCost(5, 3), self::equalTo(5));
-        self::assertThat($target->isExistRoute(3, 5), self::isTrue());
-        self::assertThat($target->isExistRoute(5, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 3), self::isTrue());
 
-        self::assertThat($target->isExistRoute(0, 4), self::isFalse());
-        self::assertThat($target->isExistRoute(4, 0), self::isFalse());
-        self::assertThat($target->isExistRoute(1, 5), self::isFalse());
-        self::assertThat($target->isExistRoute(5, 1), self::isFalse());
-        self::assertThat($target->isExistRoute(2, 4), self::isFalse());
-        self::assertThat($target->isExistRoute(4, 2), self::isFalse());
+        self::assertThat($target->isPathExist(0, 4), self::isFalse());
+        self::assertThat($target->isPathExist(4, 0), self::isFalse());
+        self::assertThat($target->isPathExist(1, 5), self::isFalse());
+        self::assertThat($target->isPathExist(5, 1), self::isFalse());
+        self::assertThat($target->isPathExist(2, 4), self::isFalse());
+        self::assertThat($target->isPathExist(4, 2), self::isFalse());
     }
 
     /**
@@ -149,47 +143,49 @@ class WarshallFloydTest extends TestCase
         $target->addEdge(3, 5, 5);
         self::assertThat($target->getCost(0, 1), self::equalTo(10));
         self::assertThat($target->getCost(1, 0), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(0, 1), self::isTrue());
-        self::assertThat($target->isExistRoute(1, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 0), self::isFalse());
         self::assertThat($target->getCost(0, 2), self::equalTo(16));
         self::assertThat($target->getCost(2, 0), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(0, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 0), self::isFalse());
         self::assertThat($target->getCost(0, 3), self::equalTo(12));
         self::assertThat($target->getCost(3, 0), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(0, 3), self::isTrue());
-        self::assertThat($target->isExistRoute(3, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 0), self::isFalse());
         self::assertThat($target->getCost(1, 2), self::equalTo(18));
         self::assertThat($target->getCost(2, 1), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(1, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 1), self::isFalse());
+        self::assertThat($target->isPathExist(1, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 1), self::isFalse());
         self::assertThat($target->getCost(3, 2), self::equalTo(3));
         self::assertThat($target->getCost(2, 3), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(3, 2), self::isTrue());
-        self::assertThat($target->isExistRoute(2, 3), self::isFalse());
+        self::assertThat($target->isPathExist(3, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 3), self::isFalse());
         self::assertThat($target->getCost(1, 4), self::equalTo(4));
         self::assertThat($target->getCost(4, 1), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(1, 4), self::isTrue());
-        self::assertThat($target->isExistRoute(4, 1), self::isFalse());
+        self::assertThat($target->isPathExist(1, 4), self::isTrue());
+        self::assertThat($target->isPathExist(4, 1), self::isFalse());
         self::assertThat($target->getCost(2, 5), self::equalTo(1));
         self::assertThat($target->getCost(5, 2), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(2, 5), self::isTrue());
-        self::assertThat($target->isExistRoute(5, 2), self::isFalse());
+        self::assertThat($target->isPathExist(2, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 2), self::isFalse());
         self::assertThat($target->getCost(3, 5), self::equalTo(5));
         self::assertThat($target->getCost(5, 3), self::equalTo(WarshallFloyd::INF_COST));
-        self::assertThat($target->isExistRoute(3, 5), self::isTrue());
-        self::assertThat($target->isExistRoute(5, 3), self::isFalse());
+        self::assertThat($target->isPathExist(3, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 3), self::isFalse());
 
-        self::assertThat($target->isExistRoute(0, 4), self::isFalse());
-        self::assertThat($target->isExistRoute(4, 0), self::isFalse());
-        self::assertThat($target->isExistRoute(1, 5), self::isFalse());
-        self::assertThat($target->isExistRoute(5, 1), self::isFalse());
-        self::assertThat($target->isExistRoute(2, 4), self::isFalse());
-        self::assertThat($target->isExistRoute(4, 2), self::isFalse());
+        self::assertThat($target->isPathExist(0, 4), self::isFalse());
+        self::assertThat($target->isPathExist(4, 0), self::isFalse());
+        self::assertThat($target->isPathExist(1, 5), self::isFalse());
+        self::assertThat($target->isPathExist(5, 1), self::isFalse());
+        self::assertThat($target->isPathExist(2, 4), self::isFalse());
+        self::assertThat($target->isPathExist(4, 2), self::isFalse());
     }
 
     /**
      * @covers WarshallFloyd::calc
+     * @covers WarshallFloyd::getCost
+     * @covers WarshallFloyd::isPathExist
      */
     public function testCalc_無向グラフの場合(): void
     {
@@ -227,8 +223,33 @@ class WarshallFloydTest extends TestCase
         self::assertThat($target->getCost(5, 0), self::equalTo(16));
         self::assertThat($target->getCost(2, 4), self::equalTo(22));
         self::assertThat($target->getCost(4, 2), self::equalTo(22));
+        self::assertThat($target->isPathExist(0, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 0), self::isTrue());
+        self::assertThat($target->isPathExist(0, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 0), self::isTrue());
+        self::assertThat($target->isPathExist(0, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 0), self::isTrue());
+        self::assertThat($target->isPathExist(1, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 1), self::isTrue());
+        self::assertThat($target->isPathExist(3, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 3), self::isTrue());
+        self::assertThat($target->isPathExist(1, 4), self::isTrue());
+        self::assertThat($target->isPathExist(4, 1), self::isTrue());
+        self::assertThat($target->isPathExist(2, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 2), self::isTrue());
+        self::assertThat($target->isPathExist(3, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 3), self::isTrue());
+        self::assertThat($target->isPathExist(0, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 0), self::isTrue());
+        self::assertThat($target->isPathExist(2, 4), self::isTrue());
+        self::assertThat($target->isPathExist(4, 2), self::isTrue());
     }
 
+    /**
+     * @covers WarshallFloyd::calc
+     * @covers WarshallFloyd::getCost
+     * @covers WarshallFloyd::isPathExist
+     */
     public function testCalc_有効グラフの場合(): void
     {
         $target = new WarshallFloyd(6, true);
@@ -265,10 +286,63 @@ class WarshallFloydTest extends TestCase
         self::assertThat($target->getCost(5, 0), self::equalTo(WarshallFloyd::INF_COST));
         self::assertThat($target->getCost(2, 4), self::equalTo(WarshallFloyd::INF_COST));
         self::assertThat($target->getCost(4, 2), self::equalTo(WarshallFloyd::INF_COST));
+        self::assertThat($target->isPathExist(0, 1), self::isTrue());
+        self::assertThat($target->isPathExist(1, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 0), self::isFalse());
+        self::assertThat($target->isPathExist(0, 3), self::isTrue());
+        self::assertThat($target->isPathExist(3, 0), self::isFalse());
+        self::assertThat($target->isPathExist(1, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 1), self::isFalse());
+        self::assertThat($target->isPathExist(3, 2), self::isTrue());
+        self::assertThat($target->isPathExist(2, 3), self::isFalse());
+        self::assertThat($target->isPathExist(1, 4), self::isTrue());
+        self::assertThat($target->isPathExist(4, 1), self::isFalse());
+        self::assertThat($target->isPathExist(2, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 2), self::isFalse());
+        self::assertThat($target->isPathExist(3, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 3), self::isFalse());
+        self::assertThat($target->isPathExist(0, 5), self::isTrue());
+        self::assertThat($target->isPathExist(5, 0), self::isFalse());
+        self::assertThat($target->isPathExist(2, 4), self::isFalse());
+        self::assertThat($target->isPathExist(4, 2), self::isFalse());
     }
 
-    public function testGetPath(): void
+    /**
+     * @covers WarshallFloyd::calc
+     * @covers WarshallFloyd::getPath
+     */
+    public function testGetPath_無向グラフの場合(): void
     {
-        self::markTestIncomplete();
+        $target = new WarshallFloyd(3, false);
+        $target->addEdge(0, 1, 1);
+        $target->addEdge(0, 2, 1);
+        $target->addEdge(1, 2, 3);
+        $target->calc();
+        self::assertThat($target->getPath(0, 1), self::equalTo([0, 1]));
+        self::assertThat($target->getPath(0, 2), self::equalTo([0, 2]));
+        self::assertThat($target->getPath(1, 2), self::equalTo([1, 0, 2]));
+        self::assertThat($target->getPath(1, 0), self::equalTo([1, 0]));
+        self::assertThat($target->getPath(2, 0), self::equalTo([2, 0]));
+        self::assertThat($target->getPath(2, 1), self::equalTo([2, 0, 1]));
+    }
+
+    /**
+     * @covers WarshallFloyd::calc
+     * @covers WarshallFloyd::getPath
+     */
+    public function testGetPath_有効グラフの場合(): void
+    {
+        $target = new WarshallFloyd(3, true);
+        $target->addEdge(0, 1, 1);
+        $target->addEdge(0, 2, 1);
+        $target->addEdge(1, 2, 3);
+        $target->calc();
+        self::assertThat($target->getPath(0, 1), self::equalTo([0, 1]));
+        self::assertThat($target->getPath(0, 2), self::equalTo([0, 2]));
+        self::assertThat($target->getPath(1, 2), self::equalTo([1, 2]));
+        self::assertThat($target->getPath(1, 0), self::equalTo([]));
+        self::assertThat($target->getPath(2, 0), self::equalTo([]));
+        self::assertThat($target->getPath(2, 1), self::equalTo([]));
     }
 }
